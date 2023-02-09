970a09561260068369c834e3f37cac53d38f8d43
root <root@yoj-mysql-client.0ozj5fusxuxeviawpb2que5alh.ax.internal.cloudapp.net>
2023-02-09 10:49:47 +0000

    update code for compile and cmake files
    Signed-off-by: root <root@yoj-mysql-client.0ozj5fusxuxeviawpb2que5alh.ax.internal.cloudapp.net>

--- compile.sh
+++ compile.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cnt=`dpkg -l | grep build-essential | wc -l`
+if [[ $cnt -eq 0 ]]; then
+apt update
+apt install -y build-essential cmake
+apt-get install build-essential cmake zlib1g-dev libssl-dev libgtest-dev -y
+fi
+
 git submodule update --init --recursive
 make
 
