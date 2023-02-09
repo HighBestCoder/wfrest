5c5fb74882ac307c5b00bcb7bb0eb84988e99f22
root <root@yoj-mysql-client.0ozj5fusxuxeviawpb2que5alh.ax.internal.cloudapp.net>
2023-02-09 13:47:05 +0000

    update api for file compare
    Signed-off-by: root <root@yoj-mysql-client.0ozj5fusxuxeviawpb2que5alh.ax.internal.cloudapp.net>

--- file_compare/main.cc
+++ file_compare/main.cc
@@ -2,6 +2,8 @@
 #include "wfrest/HttpServer.h"
 #include "wfrest/json.hpp"
 
+#include "dc_api.h"
+
 #include <csignal>
 
 using namespace wfrest;
@@ -15,6 +17,20 @@ void sig_handler(int signo)
 
 int main()
 {
+    dc_api_ctx_idx_t ctx_idx;
+    dc_common_code_t ret;
+
+    ret = dc_api_ctx_construct(&ctx_idx);
+    if (ret != S_SUCCESS) {
+        fprintf(stderr, "dc_api_ctx_construct failed\n");
+        return -1;
+    }
+
+    // use shared ptr as defer to release ctx
+    std::shared_ptr<void> defer(nullptr, [&ctx_idx](void *) {
+        dc_api_ctx_destroy(&ctx_idx);
+    });
+
     signal(SIGINT, sig_handler);
 
     HttpServer svr;
@@ -24,38 +40,34 @@ int main()
     // 3.
     // echo '{"testgzip": "gzip compress data"}' | gzip | curl -v -i --data-binary @- -H "Content-Encoding: gzip" http://localhost:8888/task
     // echo '{"testgzip": "gzip compress data"}' | curl -v -i --data-binary @- http://localhost:8888/task
-    svr.POST("/task", [](const HttpReq *req, HttpResp *resp) {
+    svr.POST("/task/{uuid}", [&ctx_idx](const HttpReq *req, HttpResp *resp) {
         // We automatically decompress the compressed data sent from the client
         // Support gzip, br only now
+        // TODO
+        const std::string& uuid = req->param("uuid");
+        if (uuid.empty()) {
+            resp->String("uuid is empty");
+            return;
+        }
+
         if (req->content_type() != APPLICATION_JSON) {
             resp->String("NOT APPLICATION_JSON");
             return;
         }
 
         auto &body = req->body();
-        /*
-         * body_json example:
-         * {
-         *   "id": "0b4e",
-         *   "basis_server": {
-         *       "id": "1abb",
-         *       "name": "drp0",
-         *       "ip": "127.0.0.1",
-         *       "path": "/home/a/xxx"
-         *   },
-         *   "compare_server_list": [
-         *   {
-         *           "id": "1abb",
-         *           "name": "drp1",
-         *           "ip": "127.0.0.1",
-         *           "path": "/home/a/xxx"
-         *   }],
-         *   "file_store_path": "/nfs/file"
-         * }
-         */
 
         // server: 这里设置压缩，注意，已经在header里面添加了相应的信息!
         resp->set_compress(Compress::GZIP);
+
+        dc_common_code_t ret = dc_api_task_add(ctx_idx,
+                                               body.data(),
+                                               body.size(),
+                                               DC_CONFIG_TYPE_JSON);
+        if (ret != S_SUCCESS) {
+            resp->String("dc_api_task_add failed");
+            return;
+        }
 
         // 这里设置返回的内容!
         resp->String("Test for server send gzip data\n");
