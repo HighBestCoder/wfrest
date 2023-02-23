f3ebcc1c2c839c80d576ed9db849fffe65912523
root <root@yoj-build-package.uiwedxwbrmuuxgzuy3wjgop0sf.bx.internal.cloudapp.net>
2023-02-23 03:02:56 +0000

    update package system
    Signed-off-by: root <root@yoj-build-package.uiwedxwbrmuuxgzuy3wjgop0sf.bx.internal.cloudapp.net>

--- package.sh
+++ package.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-mkdir -p /opt/pkg/_lib
-mkdir -p /opt/pkg/workflow/_lib/
-mkdir -p /opt/pkg/_lib/workflow/_lib
-mkdir -p /opt/pkg/file_compare
+mkdir -p /opt/fc_pkg/_lib
+mkdir -p /opt/fc_pkg/workflow/_lib/
+mkdir -p /opt/fc_pkg/_lib/workflow/_lib
+mkdir -p /opt/fc_pkg/file_compare
 mkdir -p ./workflow/_lib
 
-cp -rf /opt/fc/file_compare/README.md /opt/pkg/file_compare/README.md
-cp -rfL ./_lib/libwfrest.so /opt/pkg/_lib/libwfrest.so
-cp -rfL ./workflow/_lib/libworkflow.so  /opt/pkg/_lib/workflow/_lib/libworkflow.so
-cp -rfL ./file_compare/main /opt/pkg/file_compare/main
-cp -rfL ./file_compare/post /opt/pkg/file_compare/post
-cp -rfL ./file_compare/get /opt/pkg/file_compare/get
-cp -rfL ./file_compare/input.json /opt/pkg/file_compare/input.json
-cp -rf Dockerfile /opt/pkg/
-cp -rfL ./workflow/_lib/libworkflow.so.0 /opt/pkg/workflow/_lib/libworkflow.so.0
+cp -rf /opt/fc/file_compare/README.md /opt/fc_pkg/file_compare/README.md
+cp -rfL ./_lib/libwfrest.so /opt/fc_pkg/_lib/libwfrest.so
+cp -rfL ./workflow/_lib/libworkflow.so  /opt/fc_pkg/_lib/workflow/_lib/libworkflow.so
+cp -rfL ./file_compare/main /opt/fc_pkg/file_compare/main
+cp -rfL ./file_compare/test_post /opt/fc_pkg/file_compare/test_post
+cp -rfL ./file_compare/test_get /opt/fc_pkg/file_compare/test_get
+cp -rfL ./file_compare/input.json /opt/fc_pkg/file_compare/input.json
+cp -rfL ./workflow/_lib/libworkflow.so.0 /opt/fc_pkg/workflow/_lib/libworkflow.so.0
 
-cd /opt/pkg
+cd /opt/fc_pkg
 
-docker build . -t file_compare
+#docker build . -t file_compare
 
-cd /opt
+cd /tmp
+mv /opt/fc_pkg ./fc
 
 ts=`date +"%Y%m%d"`
-cnt=`ls /opt/ | grep kg | grep ${ts} | wc -l`
+cnt=`ls /tmp/ | grep fc_pkg | grep ${ts} | wc -l`
 let cnt=$((cnt+1))
-tar cjf file_compare-${ts}-v${cnt}.tar.bz2 /opt/pkg
+tar cjf file_compare-${ts}-v${cnt}.tar.bz2 fc
